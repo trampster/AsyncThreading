aeca6992d6872b023890c16e7023639e230d0dea
Daniel Hughes <trampster@gmail.com>
2021-08-20 23:07:54 +1200

    Add test for the messenger.

---
+++ test/MessengerTests.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AsyncThreading.Tests
+{
+    public class MessengerTests
+    {
+        class MessageReceiver : ISubscriber<string>
+        {
+            public string ReceivedMessage {get;set;}
+
+            public int ReceivingThread {get;set;}
+
+            public Task OnMessageReceived(string message)
+            {
+                ReceivedMessage = message;
+                ReceivingThread = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                return Task.CompletedTask;
+            }
+        }
+
+        [Test]
+        public async Task Publish_OneSubscriber_HandlesMessageInSubscribersThread()
+        {
+            // arrage
+            var messenger = new Messenger();
+            AsyncThread asyncThread = new AsyncThread();
+
+            var cancellationTokenSource = new System.Threading.CancellationTokenSource();
+            var threadTask = asyncThread.Start(cancellationTokenSource.Token);
+
+            var messageReceiver  = new MessageReceiver();
+            int? subscribingThread = null;
+            await asyncThread.RunInThreadAsync(() => 
+            {
+                messenger.Subscribe<string>(messageReceiver);
+                subscribingThread = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                return Task.CompletedTask;
+            });
+
+            // act
+            messenger.Publish("hello thread");
+
+            // this will wait for the thread to be finished with the message
+            await asyncThread.RunInThreadAsync(() => Task.CompletedTask);
+
+            // assert
+            Assert.That(messageReceiver.ReceivedMessage, Is.EqualTo("hello thread"));
+            Assert.That(messageReceiver.ReceivingThread, Is.EqualTo(subscribingThread));
+
+            cancellationTokenSource.Cancel();
+            await threadTask;
+        }
+    }
+
+}
